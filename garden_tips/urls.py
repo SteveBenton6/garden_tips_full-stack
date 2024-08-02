736b5099a8d0902678b3324bb66c1ec869f0ae82
Steven Benton <steve@bentonemail.uk>
2024-08-02 12:06:37 +0000

    Doc: Add authentication.

--- garden_tips/urls.py
+++ garden_tips/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# Inlcude allows to import and use another url.py
+# Include allows to import and use another url.py
 
 urlpatterns = [
     path("about/", include("about.urls"), name="about-urls"),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("tips.urls"), name="tips-urls"),
